511d4d77eaf6fc5bc715ebf18d0ac1e6cb18d8fb
gaurav1996b <1996gauravb@gmail.com>
2022-07-29 15:39:29 +0530

    updating terraform file with roboshop project

--- vars.tf
+++ vars.tf
@@ -4,4 +4,7 @@ variable "ENV" {}
 variable "AZ" {}
 variable "PRIVATE_SUBNETS_CIDR" {}
 variable "PUBLIC_SUBNETS_CIDR" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_RT" {}
 
